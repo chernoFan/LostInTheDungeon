1d7f046a104088f1e53c76f8e896f4f7e66bf6be
chernoFan <rgbs.writer@gmail.com>
2024-03-09 11:02:27 +0800

    Player Class

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include "hello_world.hpp"
+#include "Player.hpp"
 
 int main() {
-  hello_world();
+  Player* player = new Player();
+
   std::cin.get();
+
+  delete player;
   return 0;
 }
---
+++ include/Player.hpp
@@ -0,0 +1,30 @@
+#ifndef _PLAYER_HPP_
+#define _PLAYER_HPP_
+
+#include <string>
+
+class Player {
+private:
+  std::string m_name;
+  int m_level;
+  int m_money;
+public:
+  Player()
+    : m_name(""), m_level(0), m_money(0) {};
+  
+  Player(std::string name, int level, int money)
+    : m_name(name), m_level(level), m_money(money) {};
+
+  // getters setters
+  void set_player_name(std::string name) { this->m_name = name; }
+  std::string get_player_name() { return this->m_name; }
+
+  void set_player_level(int level) { this->m_level = level; }
+  int get_player_level() { return this->m_level; }
+
+  void set_player_money(int money) { this->m_money = money; }
+  int get_player_money() { return this->m_money; }
+
+};
+
+#endif
